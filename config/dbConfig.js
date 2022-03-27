78cfa84d82c440efee0023b3732d589dd43e8cf6
gayetlyan <90770999+gayetlyan@users.noreply.github.com>
2022-03-28 00:14:40 +0200

    commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,12 +5,14 @@ const mysql = require('mysql')
 
 //creation de ma connnexion mysql 
 const dbConn = mysql.createConnection({
-    host  : 'localhost',
-    user : 'root',
-    password : 'root',
-    database : 'eatalie',
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    host  : 'eu-cdbr-west-02.cleardb.net',
+    user : 'b1076e7d6cd4df',
+    password : 'b28d2263',
+    database : 'heroku_a2bdcf1d5d9b873',
+    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
+
+
 
 dbConn.connect(function(error){
     if(error) throw error
